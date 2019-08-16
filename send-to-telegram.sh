b29f11c32a82202d4608ce3e8d407bf5c31730ce
Sarsa Murmu <sarsamurmu05@gmail.com>
2019-08-16 21:33:55 +0530

    Update send-to-telegram.sh

--- send-to-telegram.sh
+++ send-to-telegram.sh
@@ -4,6 +4,9 @@ if [ "$TRAVIS_PULL_REQUEST" = false ]; then
   for apk in $(find *.apk -type f); do
     apkName="${apk}"
     echo $apkName
-    curl -v -F document=@"${TRAVIS_BUILD_DIR}/app/build/outputs/apk/release/${apkName}" https://api.telegram.org/bot$BOT_TOKEN/sendDocument?chat_id=@candybar_builds
+    if [ apkName = "candybar-signed.apk" ]; then
+      echo "Found Signed APK"
+      curl -v -F document=@"${TRAVIS_BUILD_DIR}/app/build/outputs/apk/release/${apkName}" https://api.telegram.org/bot$BOT_TOKEN/sendDocument?chat_id=@candybar_builds
+    fi
   done
 fi
