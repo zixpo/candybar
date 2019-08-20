2b2ba02132c8392a909e04bfb872a850dd4944f6
sarsamurmu <sarsamurmu05@gmail.com>
2019-08-20 15:15:14 +0530

    Update scripts [ci skip]

--- sign-apk.sh
+++ sign-apk.sh
@@ -1,6 +1,12 @@
 cd $TRAVIS_BUILD_DIR/app/build/outputs/apk/release/
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $TRAVIS_BUILD_DIR/app/candybar.jks -storepass candybar -keypass candybar candybar.apk key0
 jarsigner -verify candybar.apk
-TIME=`date +%d%m%Y-%H%M`
-$ANDROID_HOME/build-tools/29.0.0/zipalign -f 4 candybar.apk candybar-${TIME}.apk
+
+if [ "$TRAVIS_TAG" ]; then
+  $ANDROID_HOME/build-tools/29.0.0/zipalign -f 4 candybar.apk candybar-${TRAVIS_TAG}.apk
+else
+  TIME=`date +%d%m%Y-%H%M`
+  $ANDROID_HOME/build-tools/29.0.0/zipalign -f 4 candybar.apk candybar-${TIME}.apk
+fi
+
 rm candybar.apk
