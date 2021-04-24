ed5c46d9c7756999d93807ba789de137d5f21053
Sarsa Murmu <sarsamurmu05@gmail.com>
2021-04-24 10:07:56 +0530

    Create send-telegram.sh

--- send-telegram.sh
+++ send-telegram.sh
@@ -2,16 +2,16 @@ if [ "$TRAVIS_PULL_REQUEST" = true ]; then
   exit 0
 fi
 
-readonly local last_commit_log=$(git log -1 --pretty=format:"%s %b")
+readonly local last_commit_log=$(git log -1 --pretty=format:'%s %b')
 
-if [[ "$(echo "$last_commit_log" | grep -c "[skip apk]")" -gt 0 ]]; then
+if [[ "$(echo "$last_commit_log" | grep -c '[skip apk]')" -gt 0 ]]; then
   echo 'Found `[skip apk]` tag. Skipping APK publishing.'
   exit 0
 fi
 
 cd $TRAVIS_BUILD_DIR/app/build/outputs/apk/release/
 
-local name='candybar-'
+local name='CandyBar-'
 if [ "$TRAVIS_TAG" ]; then
   name+=TRAVIS_TAG
 else
@@ -21,5 +21,5 @@ name+='.apk'
 
 mv 'app-release.apk' $name
 
-curl -v -F document=@"${TRAVIS_BUILD_DIR}/app/build/outputs/apk/release/${name}" \
+curl -v -F document=@"$name" \
   https://api.telegram.org/bot$BOT_TOKEN/sendDocument?chat_id=@candybar_builds
