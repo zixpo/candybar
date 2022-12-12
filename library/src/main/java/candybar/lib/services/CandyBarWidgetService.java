8d03f69faf6b7103d2ec32c946d4f9749ef64b33
Donnnno <31142286+Donnnno@users.noreply.github.com>
2022-12-12 19:19:26 +0100

    Fix clock widget click on Android 12

--- library/src/main/java/candybar/lib/services/CandyBarWidgetService.java
+++ library/src/main/java/candybar/lib/services/CandyBarWidgetService.java
@@ -15,13 +15,23 @@ public class CandyBarWidgetService extends AppWidgetProvider {
 
     public void onReceive(Context context, Intent intent) {
         String act = intent.getAction();
+        int flags;
+
         if (AppWidgetManager.ACTION_APPWIDGET_UPDATE.equals(act)) {
             RemoteViews clockView = new RemoteViews(context.getPackageName(), R.layout.analog_clock);
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
                 Intent clockIntent = new Intent(AlarmClock.ACTION_SHOW_ALARMS);
                 clockIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                clockView.setOnClickPendingIntent(R.id.analog_clock, PendingIntent.getActivity(context, 0, clockIntent, 0));
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    flags = PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE;
+                }
+                else {
+                    flags = 0;
+                }
+
+                clockView.setOnClickPendingIntent(R.id.analog_clock, PendingIntent.getActivity(context, 0, clockIntent, flags));
             }
 
             AppWidgetManager.getInstance(context).updateAppWidget(intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS), clockView);
