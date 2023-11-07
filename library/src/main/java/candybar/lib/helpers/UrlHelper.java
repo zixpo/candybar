025e8af79ac78b21195caad73349193e08342516
Donno <github@onnno.nl>
2023-11-07 12:42:10 +0100

    5 more sites

--- library/src/main/java/candybar/lib/helpers/UrlHelper.java
+++ library/src/main/java/candybar/lib/helpers/UrlHelper.java
@@ -45,14 +45,22 @@ public class UrlHelper {
                 return getTintedDrawable(context, R.drawable.ic_toolbar_bluesky, color);
             case DRIBBBLE:
                 return getTintedDrawable(context, R.drawable.ic_toolbar_dribbble, color);
+            case DISCORD:
+                return getTintedDrawable(context, R.drawable.ic_toolbar_discord, color);                
             case FACEBOOK:
                 return getTintedDrawable(context, R.drawable.ic_toolbar_facebook, color);
             case GITHUB:
                 return getTintedDrawable(context, R.drawable.ic_toolbar_github, color);
+            case GITLAB:
+                return getTintedDrawable(context, R.drawable.ic_toolbar_gitlab, color);
+            case GOOGLEPLAY:
+                return getTintedDrawable(context, R.drawable.ic_toolbar_googleplay, color);
             case INSTAGRAM:
                 return getTintedDrawable(context, R.drawable.ic_toolbar_instagram, color);
             case MASTODON:
                 return getTintedDrawable(context, R.drawable.ic_toolbar_mastodon, color);
+            case MATRIX:
+                return getTintedDrawable(context, R.drawable.ic_toolbar_matrix, color);
             case PINTEREST:
                 return getTintedDrawable(context, R.drawable.ic_toolbar_pinterest, color);
             case THREADS:
@@ -61,6 +69,8 @@ public class UrlHelper {
                 return getTintedDrawable(context, R.drawable.ic_toolbar_x, color);
             case TELEGRAM:
                 return getTintedDrawable(context, R.drawable.ic_toolbar_telegram, color);
+            case TIKTOK:
+                return getTintedDrawable(context, R.drawable.ic_toolbar_tiktok, color);
             default:
                 return getTintedDrawable(context, R.drawable.ic_toolbar_website, color);
         }
@@ -81,14 +91,22 @@ public class UrlHelper {
             return Type.BLUESKY;
         } else if (url.contains("dribbble.")) {
             return Type.DRIBBBLE;
+        } else if (url.contains("discord.")) {
+            return Type.DISCORD;
         } else if (url.contains("facebook.")) {
             return Type.FACEBOOK;
         } else if (url.contains("github.")) {
             return Type.GITHUB;
+        } else if (url.contains("gitlab.")) {
+            return Type.GITLAB;
+        } else if (url.contains("play.google.")) {
+            return Type.GOOGLEPLAY;
         } else if (url.contains("instagram.")) {
             return Type.INSTAGRAM;
-        } else if (url.contains("mastodon.")|| url.contains("mstdn.")|| url.contains("mas.")|| url.contains("fosstodon.")|| url.contains("troet.")|| url.contains("chaos.")|| url.contains("floss.")) {
+        } else if (url.contains("mastodon.")|| url.contains("mstdn.")|| url.contains("mas.")|| url.contains("todon.")|| url.contains("fosstodon.")|| url.contains("troet.")|| url.contains("chaos.")|| url.contains("floss.")) {
             return Type.MASTODON;
+        } else if (url.contains("matrix.")) {
+            return Type.MATRIX;
         } else if (url.contains("pinterest.")) {
             return Type.PINTEREST;
         } else if (url.contains("twitter.")|| url.contains("x.")) {
@@ -97,6 +115,8 @@ public class UrlHelper {
             return Type.THREADS;
         } else if (url.contains("t.me/") || url.contains("telegram.me/")) {
             return Type.TELEGRAM;
+        } else if (url.contains("tiktok.")) {
+            return Type.TIKTOK;
         } else {
             return Type.UNKNOWN;
         }
@@ -107,14 +127,19 @@ public class UrlHelper {
         BEHANCE,
         BLUESKY,
         DRIBBBLE,
+        DISCORD,
         FACEBOOK,
         GITHUB,
+        GITLAB,
+        GOOGLEPLAY,
         INSTAGRAM,
         MASTODON,
+        MATRIX,
         PINTEREST,
         THREADS,
         TWITTER,
         TELEGRAM,
+        TIKTOK,
         UNKNOWN,
         INVALID
     }
