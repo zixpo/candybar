2ec78069474fef5291d48b9e2545d8bd0fc787b5
GitGitro <108683123+GitGitro@users.noreply.github.com>
2025-02-06 18:07:54 +0100

    remove unused import

--- library/src/main/java/candybar/lib/helpers/LauncherHelper.java
+++ library/src/main/java/candybar/lib/helpers/LauncherHelper.java
@@ -8,7 +8,6 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
-import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.DrawableRes;
